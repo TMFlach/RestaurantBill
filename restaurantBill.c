67917867c46069ef73aa752f8169755c54db9db6
Timothy Flach <tmflach@cpp.edu>
2022-10-23 10:25:00 -0700

    Added validation for # of command line arguements.

--- restaurantBill.c
+++ restaurantBill.c
@@ -1,7 +1,14 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 
 //create main function to recieve commandline arguments of tax and tip
+int main(int argc, char* argv[]) {
+    if(argc != 3) {
+        printf("Invalid # of arguments. Usage: program.exe tax tip");
+        return EXIT_FAILURE;
+    }
 
+}
 
 //create random function to select a menu item
 
