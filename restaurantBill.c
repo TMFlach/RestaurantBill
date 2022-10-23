3c5bfaf001c26fb96fdc7f0686dc83282ebf611e
Timothy Flach <tmflach@cpp.edu>
2022-10-23 09:02:32 -0700

    Renamed files to fix spelling errors.

---
+++ restaurantBill.c
@@ -0,0 +1,9 @@
+
+
+//create main function to recieve commandline arguments of tax and tip
+
+
+//create random function to select a menu item
+
+
+//create function to display the meal price, tax amount, tip amount, and total bill to screen.
